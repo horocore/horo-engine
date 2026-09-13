36a7042deae068a211ea1bc1efbc33a824092305
Abdullah Bodur <abdullahbodur.abbdr@gmail.com>
2026-09-14 01:17:41 +0200

    feat(save): model storage quota policy HORO-1428 #1428 [SAV-003.10]

--- cmake/HoroPublicHeaderOwnership.cmake
+++ cmake/HoroPublicHeaderOwnership.cmake
@@ -123,6 +123,7 @@ horo_configure_target_header_boundary(HoroRuntime PUBLIC_HEADERS
     Horo/Runtime/Save/SaveSlotIndex.h
     Horo/Runtime/Save/SaveStorageAdapter.h
     Horo/Runtime/Save/SaveSlotCommitTransaction.h
+    Horo/Runtime/Save/SaveStoragePolicy.h
     Horo/Runtime/Save/SaveOperation.h
     Horo/Runtime/Save/SaveOperationArbiter.h
     Horo/Runtime/Save/SaveProjectPolicy.h
