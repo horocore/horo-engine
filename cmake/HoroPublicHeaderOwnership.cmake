2dcdd229e1cd43d642430bf1cb0a15907140a645
Abdullah Bodur <abdullahbodur.abbdr@gmail.com>
2026-09-20 12:37:31 +0200

    feat(audio): add realtime DSP process contract HORO-565 #565 [AUD-004.4]

--- cmake/HoroPublicHeaderOwnership.cmake
+++ cmake/HoroPublicHeaderOwnership.cmake
@@ -256,6 +256,7 @@ horo_configure_target_header_boundary(HoroAudioImport PUBLIC_HEADERS
     Horo/Audio/AudioSourceImporter.h
 )
 horo_configure_target_header_boundary(HoroAudioDsp PUBLIC_HEADERS
+    Horo/Audio/AudioDSPNode.h
     Horo/Audio/AudioResampler.h
 )
 horo_configure_target_header_boundary(HoroAudioMemory PUBLIC_HEADERS
